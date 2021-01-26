538c00ba477f554b089824eed6938b05498dcdef
muhammad_aown <aown619@gmail.com>
2021-01-26 15:44:36 -0500

    back-end

---
+++ QuickLearner/src/main/java/com/example/QuickLearner/QuickLearnerApplication.java
@@ -0,0 +1,13 @@
+package com.example.QuickLearner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickLearnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickLearnerApplication.class, args);
+	}
+
+}
---
+++ QuickLearner/src/test/java/com/example/QuickLearner/QuickLearnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QuickLearner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickLearnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
